d9e861eacf29e314a81649b2ec6ae496b3febaa6
air <asilkhan-al@mail.ru>
2019-06-13 11:37:57 +0600

    Started to change

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
-        $this->call(CompanyInformationsTableSeeder::class);
         $this->call(CompanyTypesTableSeeder::class);
+        $this->call(CompanyInformationsTableSeeder::class);
     }
 }
